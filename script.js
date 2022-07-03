92867bb8de393ff0c5927b9f1b9fdd831c1b7ef3
Suda94 <100795708+Suda94@users.noreply.github.com>
2022-07-03 02:58:20 +0100

    dd

---
+++ script.js
@@ -0,0 +1,15 @@
+//current date
+n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+
+/////scrolling opening menu
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
